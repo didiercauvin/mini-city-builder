56d3345b2759b5445781357e1498ff83b5143d61
didier <didier.cauvin@gmail.com>
2025-03-08 15:01:12 +0100

    projet front

---
+++ MiniCityBuilder.Orleans.WebApi/Program.cs
@@ -0,0 +1,20 @@
+﻿using static Microsoft.AspNetCore.Http.TypedResults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseOrleansClient(static builder =>
+{
+    builder.UseLocalhostClustering();
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.Run();
