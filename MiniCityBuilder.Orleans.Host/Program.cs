62227a996e684406df98df817e333376d2e5308a
didier <didier.cauvin@gmail.com>
2025-03-08 14:43:06 +0100

    host

---
+++ MiniCityBuilder.Orleans.Host/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .UseOrleans(siloBuilder =>
+    {
+        siloBuilder.UseLocalhostClustering();
+        siloBuilder.UseDashboard();
+
+        // siloBuilder.AddMemoryGrainStorage("roomStore");
+        // siloBuilder.AddMemoryGrainStorage("playerStore");
+    })
+    .Build();
+
+
+// Start the host
+await host.StartAsync();
+
+Console.WriteLine("Orleans is running...\n\nPress enter to stop silo");
+
+Console.ReadLine();
+
+await host.StopAsync();
