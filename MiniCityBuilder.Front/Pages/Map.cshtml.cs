3e35b9f7adc95c06aedfcd49e87551b33828004f
didier.cauvin <didier.cauvin@mediane.tm.fr>
2025-03-12 17:45:02 +0100

    configuration pixijs

--- MiniCityBuilder.Front/Pages/Map.cshtml.cs
+++ MiniCityBuilder.Front/Pages/Map.cshtml.cs
@@ -18,33 +18,6 @@ public class Map : PageModel
             return RedirectToPage("/Index");
         }
 
-        var userNameFromToken = User.Identity.Name;
-        var userNameFromQuery = Request.Query["player"];
-
-        //if (userNameFromToken != userNameFromQuery)
-        //{
-        //    return Redirect("/Index");
-        //}
-
-
-        // Récupère le joueur depuis la session
-        //var playerJson = HttpContext.Session.GetString("PLAYER");
-        //if (string.IsNullOrEmpty(playerJson))
-        //{
-        //    return RedirectToPage("/Index"); // Redirige si pas de session
-        //}
-
-        //// Désérialise l'objet
-        //var player = JsonSerializer.Deserialize<PlayerDto>(playerJson);
-
-        //// Vérifie si le token est valide
-        //if (!IsTokenValid(player.Token))
-        //{
-        //    HttpContext.Session.Remove("PLAYER"); // Supprime la session expirée
-        //    return RedirectToPage("/Index"); // Redirige vers la page de login
-        //}
-
-        //Username = player.UserName;
         return Page();
     }
 
